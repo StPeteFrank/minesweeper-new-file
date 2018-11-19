3bb28c208ccd14fcb69a026d4f91f5d435e219ff
Frank Diaz <diazfrankp@gmail.com>
2018-11-19 16:10:48 -0500

    Restart with game number and number of mines.

--- src/App.js
+++ src/App.js
@@ -7,29 +7,48 @@ class App extends Component {
     super(props)
     // This is raw data from API. Had to put it on json file to compress it.
     this.state = {
+      playing: false,
       game: {
         id: 1,
         board: [
-          [' *', ' ', '2 ', ' ', ' F', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', 'F ', ' ', ' ', ' '],
           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' ', ' F', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+          [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
           [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
         ],
         state: 'new',
-        mines: 7
+        mines: 10
       }
     }
   }
+  // This event will pull API data /games then respond with the setState of response data. Use the newGame API to request new game.
   newGame = event => {
     axios.post('https://minesweeper-api.herokuapp.com/games').then(response => {
       this.setState({
+        playing: true,
         game: response.data
       })
     })
+  }
+  // If else statement allowing for either number of game or Start New Game to display.
+  headerText = () => {
+    if (this.state.playing) {
+      return `Game #${this.state.game.id}`
+    } else {
+      return 'Start a new game!'
+    }
+  }
+  //Display the number of the game we are playing
+  minesText = () => {
+    if (this.state.playing) {
+      return `${this.state.game.mines} mines left`
+    } else {
+      return ''
+    }
   }
 
   render() {
@@ -38,7 +57,7 @@ class App extends Component {
         <table>
           <tbody>
             <tr>
-              {/* This is a selector inside the header. Theres also a button which will become the restart. */}
+              {/* This is a selector inside the header. The restart button is also here. */}
               <td className="header" colSpan="8">
                 <select>
                   <option value="0">Easy</option>
@@ -50,7 +69,7 @@ class App extends Component {
             </tr>
             <tr>
               <td className="header not-playing" colSpan="8">
-                Start a new game!
+                {this.headerText()}
               </td>
             </tr>
             <tr>
@@ -135,7 +154,7 @@ class App extends Component {
             </tr>
             <tr>
               <td className="header" colSpan="8">
-                {this.state.game.mines} mines left
+                {this.minesText()}
               </td>
             </tr>
           </tbody>

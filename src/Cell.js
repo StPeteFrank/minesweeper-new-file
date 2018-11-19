832773e6ee746779c1616ee419ca45b3f856a3f4
Frank Diaz <diazfrankp@gmail.com>
2018-11-19 18:10:25 -0500

    checkCellOne

--- src/Cell.js
+++ src/Cell.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 
 class Cell extends Component {
+  flagCell = event => {
+    console.log('clicked cells')
+  }
+
   render() {
-    return <td>{this.props.value}</td>
+    // This is an onClick handle
+    return <td onClick={this.flagCell}>{this.props.value}</td>
   }
 }
 

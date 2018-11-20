3e59b480176fea9597efecac98acc2d517e79af7
Frank Diaz <diazfrankp@gmail.com>
2018-11-19 19:35:29 -0500

    Right click works finally

--- src/Cell.js
+++ src/Cell.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 
 class Cell extends Component {
-  flagCell = event => {
+  checkCell = event => {
     console.log(`Clicked at row ${this.props.row} column ${this.props.col}`)
 
     this.props.checkCell(this.props.row, this.props.col)
   }
+  flagCell = event => {
+    event.preventDefault()
 
+    console.log('RIGHT CLICKED')
+  }
   render() {
-    return <td onClick={this.flagCell}>{this.props.value}</td>
+    return (
+      <td onClick={this.checkCell} onContextMenu={this.flagCell}>
+        {this.props.value}
+      </td>
+    )
   }
 }
 

8a175575d337814db6ac49b1d666eeec127f10f6
Frank Diaz <diazfrankp@gmail.com>
2018-11-19 19:12:27 -0500

    Starting checkCells again.

--- src/Cell.js
+++ src/Cell.js
@@ -3,12 +3,9 @@ import React, { Component } from 'react'
 class Cell extends Component {
   flagCell = event => {
     console.log(`Clicked at row ${this.props.row} column ${this.props.col}`)
-
-    this.props.checkCell(this.props.row, this.props.col)
   }
 
   render() {
-    // This is an onClick handle
     return <td onClick={this.flagCell}>{this.props.value}</td>
   }
 }

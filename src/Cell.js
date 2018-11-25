6cc2f94851d1094bbd31e05b79f08a3ca59cb22c
Frank Diaz <diazfrankp@gmail.com>
2018-11-24 19:00:28 -0500

    Emojis Added

--- src/Cell.js
+++ src/Cell.js
@@ -9,10 +9,39 @@ class Cell extends Component {
 
     this.props.flagCell(this.props.row, this.props.col)
   }
+  cellClassName = () => {
+    const Lookup = {
+      '*': 'cell-bomb',
+      F: 'cell-flag',
+      '@': 'cell-flag',
+      _: 'cell-covered',
+      '1': 'cell-1',
+      '2': 'cell-2',
+      '3': 'cell-3',
+      '4': 'cell-4',
+      '5': 'cell-5',
+      '6': 'cell-6',
+      '7': 'cell-7',
+      '8': 'cell-8'
+    }
+    return Lookup[this.props.value] || 'cell-exposed'
+  }
+
+  lookupValue = () => {
+    const Lookup = { '*': '🎇', F: '🚩', '@': '🚩', _: '_' }
+
+    let foundValue = Lookup[this.props.value] || this.props.value
+    return foundValue
+  }
+
   render() {
     return (
-      <td onClick={this.checkCell} onContextMenu={this.flagCell}>
-        {this.props.value}
+      <td
+        className={this.cellClassName()}
+        onClick={this.checkCell}
+        onContextMenu={this.flagCell}
+      >
+        {this.lookupValue()}
       </td>
     )
   }
